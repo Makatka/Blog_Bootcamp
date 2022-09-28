b17b0a3f907d73fae344c810befeb663b219314b
Natalia Sikoń <natalii.mazur@gmail.com>
2022-09-28 09:53:39 +0200

    Remove class active

--- js/script.js
+++ js/script.js
@@ -6,12 +6,20 @@ const links = document.querySelectorAll('.titles a');
 function titleClickHandler(e){
     console.log('Link was clicked!');
     console.log(e);
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeArticles = document.querySelectorAll('article.post.active');
 
     /* remove class 'active' from all article links  */
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    for(let acitveArticle of activeArticles){
+        acitveArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
